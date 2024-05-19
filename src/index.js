cdd95c64315b0276f01cd925cf4e090a5086f2f1
armanx0333 <armansethi64@gmail.com>
2024-05-19 18:39:56 +0530

    connect to atlasMongoDB

---
+++ src/index.js
@@ -0,0 +1,41 @@
+
+
+
+
+import { EventEmitter } from 'events';
+EventEmitter.defaultMaxListeners = 20; 
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB();
+
+/*
+import express  from "express";
+const app = express()
+(async()=>{
+    try{
+      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERRR:",error);
+            throw error
+        })
+        app.lister(process.env.PORT,()=>{
+            console.log(`App is listening on the port ${process.env.PORT}`)
+        })
+    }
+    catch(error){
+        console.log("Error :",error)
+        throw err
+    }
+})()
+
+*/
